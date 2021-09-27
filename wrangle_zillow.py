2f7fc2642cda3324aefeab74dc1371a259112cfd
jamesallen0351 <james.allen0351@gmail.com>
2021-09-27 10:40:29 -0500

    update scratchpad in progress and add to wrangle_zillow

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -1,6 +1,9 @@
 # wrangle_zillow
 
 # imports
+import warnings
+warnings.filterwarnings("ignore")
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,167 +14,291 @@ import env
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-
-sql = """
-SELECT prop.*,
-       pred.logerror,
-       pred.transactiondate,
-       air.airconditioningdesc,
-       arch.architecturalstyledesc,
-       build.buildingclassdesc,
-       heat.heatingorsystemdesc,
-       landuse.propertylandusedesc,
-       story.storydesc,
-       construct.typeconstructiondesc
-FROM   properties_2017 prop
-       INNER JOIN (SELECT parcelid,
-                   Max(transactiondate) transactiondate
-                   FROM   predictions_2017
-                   GROUP  BY parcelid) pred
-               USING (parcelid)
-               			JOIN predictions_2017 as pred USING (parcelid, transactiondate)
-       LEFT JOIN airconditioningtype air USING (airconditioningtypeid)
-       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid)
-       LEFT JOIN buildingclasstype build USING (buildingclasstypeid)
-       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid)
-       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid)
-       LEFT JOIN storytype story USING (storytypeid)
-       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid)
-WHERE  prop.latitude IS NOT NULL
-       AND prop.longitude IS NOT NULL
-"""
-
-def get_db_url(database):
-    from env import host, user, password
-    url = f'mysql+pymysql://{user}:{password}@{host}/{database}'
-    return url
+import sklearn.preprocessing
+import scipy.stats as stats
 
 
-# acquire zillow data using the query
-def get_zillow(sql):
-    url = get_db_url('zillow')
-    zillow_df = pd.read_sql(sql, url, index_col='id')
-    return zillow_df
+def get_object_cols(df):
+    '''
+    This function takes in a dataframe and identifies the columns that are object types
+    and returns a list of those column names. 
+    '''
+    # create a mask of columns whether they are object type or not
+    mask = np.array(df.dtypes == "object")
 
+        
+    # get a list of the column names that are objects (from the mask)
+    object_cols = df.iloc[:, mask].columns.tolist()
+    
+    return object_cols
 
-def handle_missing_values(df, prop_required_column = .5, prop_required_row = .70):
-	#function that will drop rows or columns based on the percent of values that are missing:\
-	#handle_missing_values(df, prop_required_column, prop_required_row
-    threshold = int(round(prop_required_column*len(df.index),0))
-    df = df.dropna(axis=1, thresh=threshold)
-    threshold = int(round(prop_required_row*len(df.columns),0))
-    df.dropna(axis=0, thresh=threshold, inplace=True)
-    return df
+def get_numeric_cols(df, object_cols):
+    '''
+    takes in a dataframe and list of object column names
+    and returns a list of all other columns names, the non-objects. 
+    '''
+    numeric_cols = [col for col in df.columns.values if col not in object_cols]
+    
+    return numeric_cols
 
-
-def remove_columns(df, cols_to_remove):
-	#remove columns not needed
-    df = df.drop(columns=cols_to_remove)
-    return df
-
-def wrangle_zillow():
-    if os.path.isfile('zillow_cached.csv') == False:
-        df = get_zillow(sql)
-        df.to_csv('zillow_cached.csv',index = False)
-    else:
-        df = pd.read_csv('zillow_cached.csv')
-
-    # Restrict df to only properties that meet single use criteria
+def get_single_use_prop(df):
     single_use = [261, 262, 263, 264, 266, 268, 273, 276, 279]
     df = df[df.propertylandusetypeid.isin(single_use)]
-
-    # Restrict df to only those properties with at least 1 bath & bed and 350 sqft area
-    df = df[(df.bedroomcnt > 0) & (df.bathroomcnt > 0) & ((df.unitcnt<=1)|df.unitcnt.isnull())\
-            & (df.calculatedfinishedsquarefeet>350)]
-
-    # Handle missing values i.e. drop columns and rows based on a threshold
-    df = handle_missing_values(df)
-
-    # Add column for counties
-    df['county'] = df['fips'].apply(
-        lambda x: 'Los Angeles' if x == 6037\
-        else 'Orange' if x == 6059\
-        else 'Ventura')
-
-    # drop unnecessary columns
-    dropcols = ['parcelid',
-         'calculatedbathnbr',
-         'finishedsquarefeet12',
-         'fullbathcnt',
-         'heatingorsystemtypeid',
-         'propertycountylandusecode',
-         'propertylandusetypeid',
-         'propertyzoningdesc',
-         'censustractandblock',
-         'propertylandusedesc']
-
-    df = remove_columns(df, dropcols)
-
-    # replace nulls in unitcnt with 1
-    df.unitcnt.fillna(1, inplace = True)
-
-    # assume that since this is Southern CA, null means 'None' for heating system
-    df.heatingorsystemdesc.fillna('None', inplace = True)
-
-    # replace nulls with median values for select columns
-    df.lotsizesquarefeet.fillna(7313, inplace = True)
-    df.buildingqualitytypeid.fillna(6.0, inplace = True)
-
-    # Columns to look for outliers
-    df = df[df.taxvaluedollarcnt < 5_000_000]
-    df = df[df.calculatedfinishedsquarefeet < 8000]
-
-    # Just to be sure we caught all nulls, drop them here
-    df = df.dropna()
-
-    return df
-
-def min_max_scaler(train, valid, test):
-    '''
-    Uses the train & test datasets created by the split_my_data function
-    Returns 3 items: mm_scaler, train_scaled_mm, test_scaled_mm
-    This is a linear transformation. Values will lie between 0 and 1
-    '''
-    num_vars = list(train.select_dtypes('number').columns)
-    scaler = MinMaxScaler()
-    train[num_vars] = scaler.fit_transform(train[num_vars])
-    valid[num_vars] = scaler.transform(valid[num_vars])
-    test[num_vars] = scaler.transform(test[num_vars])
-    return scaler, train, valid, test
-
-def outlier_function(df, cols, k):
-	#function to detect and handle oulier using IQR rule
-    for col in df[cols]:
-        q1 = df.annual_income.quantile(0.25)
-        q3 = df.annual_income.quantile(0.75)
-        iqr = q3 - q1
-        upper_bound =  q3 + k * iqr
-        lower_bound =  q1 - k * iqr
-        df = df[(df[col] < upper_bound) & (df[col] > lower_bound)]
     return df
 
 
-def train_validate_test_split(df):
+def handle_missing_values(df, prop_required_row = 0.5, prop_required_col = 0.5):
+    ''' funtcion which takes in a dataframe, required notnull proportions of non-null rows and columns.
+    drop the columns and rows columns based on theshold:'''
+    
+    #drop columns with nulls
+    threshold = int(prop_required_col * len(df.index)) # Require that many non-NA values.
+    df.dropna(axis = 1, thresh = threshold, inplace = True)
+    
+    #drop rows with nulls
+    threshold = int(prop_required_row * len(df.columns)) # Require that many non-NA values.
+    df.dropna(axis = 0, thresh = threshold, inplace = True)
+    
+    
+    return df
+
+def get_latitude(df):
+    '''
+    This function takes in a datafame with latitude formatted as a float,
+    converts it to a int and utilizes lambda to return the latitude values
+    in a correct format.
+    '''
+    df.latitude = df.latitude.astype(int)
+    df['latitude'] = df['latitude'].apply(lambda x: x / 10 ** (len((str(x))) - 2))
+    return df
+
+def get_longitude(df):
+    '''This function takes in a datafame with longitude formatted as a float,
+    converts it to a int and utilizes lambda to return the longitude values
+    in the correct format.
+    '''
+    df.longitude = df.longitude.astype(int)
+    df['longitude'] = df['longitude'].apply(lambda x: x / 10 ** (len((str(x))) - 4))
+    return df
+
+def clean_zillow(df):
+    df = get_single_use_prop(df)
+
+    df = handle_missing_values(df, prop_required_row = 0.5, prop_required_col = 0.5)
+
+    df.set_index('parcelid', inplace=True)
+
+    # cols_to_drop = ['fullbathcnt','heatingorsystemtypeid','finishedsquarefeet12', 
+    #             'propertycountylandusecode', 'propertylandusetypeid','propertyzoningdesc', 'censustractandblock',
+    #             'propertylandusedesc', 'buildingqualitytypeid' , 'unitcnt', 'heatingorsystemdesc', 
+    #             'lotsizesquarefeet','regionidcity', 'calculatedbathnbr', 'transactiondate', 'roomcnt', 'id', 'regionidcounty',
+    #             'regionidzip', 'assessmentyear']
+
+    # df.drop(columns=cols_to_drop, inplace = True)
+
+    df.dropna(inplace = True)
+
+    get_latitude(df)
+
+    get_longitude(df)
+
+    return df
+
+def get_counties(df):
+    '''
+    This function will create dummy variables out of the original fips column. 
+    And return a dataframe with all of the original columns except regionidcounty.
+    We will keep fips column for data validation after making changes. 
+    New columns added will be 'LA', 'Orange', and 'Ventura' which are boolean 
+    The fips ids are renamed to be the name of the county each represents. 
+    '''
+    # create dummy vars of fips id
+    county_df = pd.get_dummies(df.fips)
+    # rename columns by actual county name
+    county_df.columns = ['LA', 'Orange', 'Ventura']
+    # concatenate the dataframe with the 3 county columns to the original dataframe
+    df_dummies = pd.concat([df, county_df], axis = 1)
+    # drop regionidcounty and fips columns
+    # df_dummies = df_dummies.drop(columns = ['regionidcounty'])
+    return df_dummies
+
+def create_dummies(df, object_cols):
+    '''
+    This function takes in a dataframe and list of object column names,
+    and creates dummy variables of each of those columns. 
+    It then appends the dummy variables to the original dataframe. 
+    It returns the original df with the appended dummy variables. 
+    '''
+    
+    # run pd.get_dummies() to create dummy vars for the object columns. 
+    # we will drop the column representing the first unique value of each variable
+    # we will opt to not create na columns for each variable with missing values 
+    # (all missing values have been removed.)
+    dummy_df = pd.get_dummies(df[object_cols], dummy_na=False, drop_first=True)
+    
+    # concatenate the dataframe with dummies to our original dataframe
+    # via column (axis=1)
+    df = pd.concat([df, dummy_df], axis=1)
+
+    return df
+
+def create_features(df):
+    '''
+    function to create features to assist in exploration
+    '''
+    df['age'] = 2017 - df.yearbuilt
+    df['age_bin'] = pd.cut(df.age, 
+                           bins = [0, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140],
+                           labels = [0, .066, .133, .20, .266, .333, .40, .466, .533, 
+                                     .60, .666, .733, .8, .866, .933])
+
+    # create taxrate variable
+    df['taxrate'] = df.taxamount/df.taxvaluedollarcnt*100
+
+    # create acres variable
+    df['acres'] = df.lotsizesquarefeet/43560
+
+    # bin acres
+    df['acres_bin'] = pd.cut(df.acres, bins = [0, .10, .15, .25, .5, 1, 5, 10, 20, 50, 200], 
+                       labels = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9])
+
+    # square feet bin
+    df['sqft_bin'] = pd.cut(df.calculatedfinishedsquarefeet, 
+                            bins = [0, 800, 1000, 1250, 1500, 2000, 2500, 3000, 4000, 7000, 12000],
+                            labels = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+                       )
+
+    # dollar per square foot-structure
+    df['structure_dollar_per_sqft'] = df.structuretaxvaluedollarcnt/df.calculatedfinishedsquarefeet
+
+
+    df['structure_dollar_sqft_bin'] = pd.cut(df.structure_dollar_per_sqft, 
+                                             bins = [0, 25, 50, 75, 100, 150, 200, 300, 500, 1000, 1500],
+                                             labels = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+                                            )
+
+
+    # dollar per square foot-land
+    df['land_dollar_per_sqft'] = df.landtaxvaluedollarcnt/df.lotsizesquarefeet
+
+    df['lot_dollar_sqft_bin'] = pd.cut(df.land_dollar_per_sqft, bins = [0, 1, 5, 20, 50, 100, 250, 500, 1000, 1500, 2000],
+                                       labels = [0, .1, .2, .3, .4, .5, .6, .7, .8, .9]
+                                      )
+
+
+    # update datatypes of binned values to be float
+    df = df.astype({'sqft_bin': 'float64', 'acres_bin': 'float64', 'age_bin': 'float64',
+                    'structure_dollar_sqft_bin': 'float64', 'lot_dollar_sqft_bin': 'float64'})
+
+
+    # ratio of bathrooms to bedrooms
+    df['bath_bed_ratio'] = df.bathroomcnt/df.bedroomcnt
+
+    # 12447 is the ID for city of LA. 
+    # I confirmed through sampling and plotting, as well as looking up a few addresses.
+    df['cola'] = df['regionidcity'].apply(lambda x: 1 if x == 12447.0 else 0)
+
+    return df
+
+def remove_outliers(df):
+    '''
+    remove outliers in bed, bath, zip, square feet, acres & tax rate
+    '''
+
+    return df[((df.bathroomcnt <= 7) & (df.bedroomcnt <= 7) & 
+               (df.regionidzip < 100000) & 
+               (df.bathroomcnt > 0) & 
+               (df.bedroomcnt > 0) & 
+               (df.acres < 20) &
+               (df.calculatedfinishedsquarefeet < 10000) & 
+               (df.taxrate < 10)
+              )]
+
+def my_train_test_split(df):
+    # split test off, 20% of original df size. 
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
+
+    print("train size: ", train.size)
+    print("validate size: ", validate.size)
+    print("test size: ", test.size)
+    
+    return train, validate, test
+
+def my_train_validate_test_split(df):
     train_and_validate, test = train_test_split(df, train_size=0.8, random_state=123)
     train, validate = train_test_split(train_and_validate, train_size=0.75, random_state=123)
     return train, validate, test
 
+def split(df, target_var):
+    '''
+    This function takes in the dataframe and target variable name as arguments and then
+    splits the dataframe into train, validate, & test
+    '''
+    # split df into train_validate and test
+    train_validate, test = train_test_split(df, test_size=.20, random_state=123)
+    # split train_validate into train and validate 
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
 
-def nulls_by_col(df):
-    num_missing = df.isnull().sum()
-    rows = df.shape[0]
-    prcnt_miss = num_missing / rows * 100
-    cols_missing = pd.DataFrame({'num_rows_missing': num_missing, 'percent_rows_missing': prcnt_miss})
-    return cols_missing
+    # create X_train by dropping the target variable 
+    X_train = train.drop(columns=[target_var])
+    # create y_train by keeping only the target variable.
+    y_train = train[[target_var]]
 
-def nulls_by_row(df):
-    num_missing = df.isnull().sum(axis=1)
-    prcnt_miss = num_missing / df.shape[1] * 100
-    rows_missing = pd.DataFrame({'num_cols_missing': num_missing, 'percent_cols_missing': prcnt_miss})\
-    .reset_index()\
-    .groupby(['num_cols_missing', 'percent_cols_missing']).count()\
-    .rename(index=str, columns={'customer_id': 'num_rows'}).reset_index()
-    return rows_missing
+    # create X_validate by dropping the target variable 
+    X_validate = validate.drop(columns=[target_var])
+    # create y_validate by keeping only the target variable.
+    y_validate = validate[[target_var]]
+
+    # create X_test by dropping the target variable 
+    X_test = test.drop(columns=[target_var])
+    # create y_test by keeping only the target variable.
+    y_test = test[[target_var]]
+
+    partitions = [train, X_train, X_validate, X_test, y_train, y_validate, y_test]
+    
+    return partitions
+
+
+def scale_my_data(train, validate, test):
+
+    train = train.drop('logerror', axis=1)
+    validate = validate.drop('logerror', axis=1)
+    test = test.drop('logerror', axis=1)
+
+    # 1. Create the Scaling Object
+    scaler = sklearn.preprocessing.StandardScaler()
+
+    # 2. Fit to the train data only
+    scaler.fit(train)
+
+    # 3. use the object on the whole df
+    # this returns an array, so we convert to df in the same line
+    train_scaled = pd.DataFrame(scaler.transform(train))
+    validate_scaled = pd.DataFrame(scaler.transform(validate))
+    test_scaled = pd.DataFrame(scaler.transform(test))
+
+    # the result of changing an array to a df resets the index and columns
+    # for each train, validate, and test, we change the index and columns back to original values
+
+    # Train
+    train_scaled.index = train.index
+    train_scaled.columns = train.columns
+
+    # Validate
+    validate_scaled.index = validate.index
+    validate_scaled.columns = validate.columns
+
+    # Test
+    test_scaled.index = test.index
+    test_scaled.columns = test.columns
+
+    return train_scaled, validate_scaled, test_scaled
+
+def prepare_zillow(df):
+    df = get_counties(df)
+    return df
+
 
 def summarize(df):
     '''
